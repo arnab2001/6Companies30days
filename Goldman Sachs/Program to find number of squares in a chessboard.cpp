67c28905614e8841e40bd14bc43d064e90bd5a05
Arnab Chatterjee <63441472+arnab2001@users.noreply.github.com>
2022-01-05 20:28:52 +0530

    Create  Program to find number of squares in a chessboard.cpp

---
+++ Goldman Sachs/Program to find number of squares in a chessboard.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to return count of squares;
+long long int countSquares(int n)
+{
+  return (n * (n + 1) / 2) * (2 * n + 1) / 3;
+}
+ 
+// Driver Code
+int main()
+{
+ 
+  int n = 4;
+  cout << countSquares(n);
+  return 0;
+}
+ 
