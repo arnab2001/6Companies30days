b4598d7afa2f707f9311fb0526426587334eb29b
Arnab Chatterjee <63441472+arnab2001@users.noreply.github.com>
2022-01-26 02:26:12 +0530

    Create Generate Binary Numbers.cpp

---
+++ Microsoft/Generate Binary Numbers.cpp
@@ -0,0 +1,35 @@
+/Function to generate binary numbers from 1 to N using a queue.
+vector<string> generate(int N)
+    {
+	vector<string>res ;
+	queue<string>q;
+	q.push("1");
+	for(int count=0 ; count<N ; count++){
+	    string curr = q.front();
+	    res.push_back(curr);
+	    q.pop();
+	    q.push(curr + '0');
+	    q.push(curr + '1');
+	}
+	return res ;
+	
+	
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<string> ans = generate(n);
+		for(auto it:ans) cout<<it<<" ";
+		cout<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
