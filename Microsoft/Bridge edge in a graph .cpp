5646169e040c7b292ec07be9da91ef69134bde2c
Arnab Chatterjee <63441472+arnab2001@users.noreply.github.com>
2022-01-26 02:12:10 +0530

    Create  Bridge edge in a graph .cpp

---
+++ Microsoft/Bridge edge in a graph .cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+	public:
+    //Function to find if the given edge is a bridge in graph.
+     bool dfs(int src, int dest, vector<int>& vis, vector<int>adj[]){
+        if(src == dest){
+            return true;
+        }
+        vis[src] = 1;
+        for(int i : adj[src]){
+            if(!vis[i]){
+                if(dfs(i,dest,vis,adj)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    int isBridge(int V, vector<int> arr[], int c, int d) 
+    {
+         // Code here
+        vector<int>adj[V];
+        for(int i=0; i<V; i++){
+            for(auto j : arr[i]){
+                if((i == c and j == d) or (i == d or j == c)){
+                    continue;
+                }
+                else{
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        vector<int>vis(V,0);
+        if(dfs(c,d,vis,adj)){
+            return 0;
+        }
+        return 1;
+    }
+}; Bridge edge in a graph 
